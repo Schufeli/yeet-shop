b12cb2b08748c7e4604180e2008de767e98a03d9
schufeli <me@schufi.codes>
2022-01-13 13:23:19 +0100

    Removed old project

--- YeetShop.Api/lib/classes/order.ts
+++
@@ -1,9 +0,0 @@
-import { Product } from './product.ts';
-export interface Order {
-    id: string,
-    name: string,
-    surname: string,
-    email: string,
-    products: Product[],
-    totalPrice: number
-}
